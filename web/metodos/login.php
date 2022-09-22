c8599ce30180065f88e923d4b58855cee9872e1e
fprp <filipe_paz.28@hotmail.com>
2022-09-22 02:40:33 -0300

    adicionar nova sessao

--- web/metodos/login.php
+++ web/metodos/login.php
@@ -1,20 +1,16 @@
 <?php
-echo phpinfo();
-?>
-
-<?php
 
 $filme = $_POST['filme'];
 $sala = $_POST['sala'];
 $diahora = $_POST['diahora'];
+$id = 0;
 
 $strcon = mysqli_connect('mysql-container','root','admin','cinema') or die('Erro ao conectar ao banco de dados');
 $sql = "INSERT INTO filmes VALUES ";
-$sql .= "('$filme', '$sala', '$diagora')"; 
-mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
+$sql .= "($id, '$filme', $sala, '$diahora')"; 
+mysqli_query($strcon,$sql) or die("Erro ao adicionar nova sessão");
 mysqli_close($strcon);
 echo "Sessão cadastradada com sucesso!";
 echo "<a href='formulario.html'>Clique aqui para realizar um novo cadastro</a><br>";
 echo "<a href='consulta.php'>Clique aqui para realizar uma consulta</a><br>";
-?>
 ?>
